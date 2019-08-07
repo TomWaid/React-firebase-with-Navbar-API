c89523245031d89c105d5d5e6fb5b934e3ad20df
Tommy <53874779+TomWaid@users.noreply.github.com>
2019-08-07 17:28:51 -0500

    Add files via upload

---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './home.css';
+
+
+const BackgroundImagePage2 = () => {
+  return (
+      <div className="bg2">This is the website</div>
+  );
+}
+
+export default BackgroundImagePage2;
